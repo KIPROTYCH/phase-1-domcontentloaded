48305530f6d0a46e7da65328385b1cc5825b4ffb
KIPROTYCH <kiprotych@gmail.com>
2023-06-23 10:55:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// // Your code goes here
+
+// Wait for DOM content to load
+document.addEventListener('DOMContentLoaded', function() {
+  // Find the element with the specified ID
+  const textElement = document.getElementById('text');
+
+  // Change the text content
+  textElement.textContent = 'This is really cool!';
+});
+
